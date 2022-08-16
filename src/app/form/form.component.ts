b8cf4099c75003b2ba158b5f32073d4211884de8
Oleksii Akastolov <akastolov1@gmail.com>
2022-08-16 13:43:48 +0300

    component driven form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,33 +1,17 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {User} from "../user";
-import {NgForm, ValidationErrors} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit, AfterViewInit {
+export class FormComponent implements OnInit {
 
-  roles: string[] = ['Гость', 'Модератор', 'Администратор']
-  user: User = new User(1, null, null, null)
-
-  formErrors: any = {
-    name: '',
-    age: ''
-  }
-
-  validationMessages: any = {
-    name: {
-      required: 'Имя обязательно',
-      minlength: 'Минимальная длина 2 символа'
-    },
-    age: {
-      required: 'Возраст обязателен'
-    }
-  }
-
-  @ViewChild('userForm') userForm!: NgForm
+  userForm = new FormGroup({
+    name: new FormControl('user', Validators.required),
+    password: new FormControl('123', [Validators.required, Validators.minLength(8)])
+  })
 
   constructor() {
   }
@@ -35,27 +19,52 @@ export class FormComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
   }
 
-  ngAfterViewInit() {
-    this.userForm.valueChanges?.subscribe(data => this.onValueChanged())
-  }
-
   onSubmit(): void {
     console.log('Form submitted')
   }
-
-  private onValueChanged(): void {
-    const form = this.userForm.form
-
-    Object.keys(this.formErrors).forEach(field => {
-      const control = form.get(field)
-      this.formErrors[field] = ''
-
-      if (control && control.dirty && control.invalid) {
-        const messages = this.validationMessages[field]
-        Object.keys(control.errors as ValidationErrors).forEach(key => {
-          this.formErrors[field] += messages[key] + ' '
-        })
-      }
-    })
-  }
 }
+
+
+//
+// roles: string[] = ['Гость', 'Модератор', 'Администратор']
+// user: User = new User(1, null, null, null)
+//
+// formErrors: any = {
+//   name: '',
+//   age: ''
+// }
+//
+// validationMessages: any = {
+//   name: {
+//     required: 'Имя обязательно',
+//     minlength: 'Минимальная длина 2 символа'
+//   },
+//   age: {
+//     required: 'Возраст обязателен'
+//   }
+// }
+//
+//
+
+//
+// ngAfterViewInit() {
+//   this.userForm.valueChanges?.subscribe(data => this.onValueChanged())
+// }
+//
+
+//
+// private onValueChanged(): void {
+//   const form = this.userForm.form
+//
+//   Object.keys(this.formErrors).forEach(field => {
+//     const control = form.get(field)
+//     this.formErrors[field] = ''
+//
+//     if (control && control.dirty && control.invalid) {
+//       const messages = this.validationMessages[field]
+//       Object.keys(control.errors as ValidationErrors).forEach(key => {
+//         this.formErrors[field] += messages[key] + ' '
+//       })
+//     }
+//   })
+// }
