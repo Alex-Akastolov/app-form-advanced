a7f8c7088f051de5f44602368056e57d73b7a35b
Oleksii Akastolov <akastolov1@gmail.com>
2022-08-25 14:40:13 +0300

    reactive form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {User} from "../user";
 
 @Component({
   selector: 'app-form',
@@ -8,63 +9,84 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class FormComponent implements OnInit {
 
-  userForm = new FormGroup({
-    name: new FormControl('user', Validators.required),
-    password: new FormControl('123', [Validators.required, Validators.minLength(8)])
-  })
+  userForm!: FormGroup
 
-  constructor() {
+  roles: string[] = ['Гость', 'Модератор', 'Администратор']
+  user: User = new User(1, null, null, null, null, null)
+
+  formErrors: any = {
+    name: '',
+    password: '',
+    email: '',
+    role: '',
+    age: ''
+  }
+
+  validationMessages: any = {
+    name: {
+      required: 'Имя обязательно',
+      minlength: 'Минимальная длина 2 символа',
+      maxlength: 'Максимальная длина 10 символа'
+    },
+    password: {
+      required: 'Пароль обязателен',
+      minlength: 'Минимальная длина 6 символов',
+      maxlength: 'Максимальная длина 15 символов'
+    },
+    email: {
+      required: 'Email обязателен',
+      pattern: 'Неправильный формат email'
+    },
+    age: {
+      required: 'Возраст обязателен',
+      pattern: 'Значение должно быть целым числом'
+    },
+    role: {
+      required: 'Роль обязательна'
+    }
+  }
+
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.buildForm()
   }
 
   onSubmit(): void {
     console.log('Form submitted')
   }
+
+  private buildForm(): void {
+    this.userForm = this.fb.group({
+      name: [this.user.name, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      password: [this.user.password, [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+      email: [this.user.email, [Validators.required, Validators.pattern(/^([a-zA-Z0-9_.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})$/)]],
+      age: [this.user.age, [Validators.required, Validators.pattern(/^\d+$/)]],
+      role: [this.user.role, [Validators.required]]
+    })
+
+    this.userForm.valueChanges.subscribe(() => this.onValueChanged())
+  }
+
+
+  private onValueChanged(): void {
+    const form = this.userForm
+
+    Object.keys(this.formErrors).forEach(field => {
+      const control = form.get(field)
+      this.formErrors[field] = ''
+
+      if (control && control.dirty && control.invalid) {
+        const messages = this.validationMessages[field]
+        Object.keys(control.errors as ValidationErrors).forEach(key => {
+          this.formErrors[field] += messages[key] + ' '
+        })
+      }
+    })
+  }
+
 }
 
-
+// RegExp for Email
 //
-// roles: string[] = ['Гость', 'Модератор', 'Администратор']
-// user: User = new User(1, null, null, null)
-//
-// formErrors: any = {
-//   name: '',
-//   age: ''
-// }
-//
-// validationMessages: any = {
-//   name: {
-//     required: 'Имя обязательно',
-//     minlength: 'Минимальная длина 2 символа'
-//   },
-//   age: {
-//     required: 'Возраст обязателен'
-//   }
-// }
-//
-//
-
-//
-// ngAfterViewInit() {
-//   this.userForm.valueChanges?.subscribe(data => this.onValueChanged())
-// }
-//
-
-//
-// private onValueChanged(): void {
-//   const form = this.userForm.form
-//
-//   Object.keys(this.formErrors).forEach(field => {
-//     const control = form.get(field)
-//     this.formErrors[field] = ''
-//
-//     if (control && control.dirty && control.invalid) {
-//       const messages = this.validationMessages[field]
-//       Object.keys(control.errors as ValidationErrors).forEach(key => {
-//         this.formErrors[field] += messages[key] + ' '
-//       })
-//     }
-//   })
-// }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,9 @@
 export class User {
   constructor(public id: number,
               public name: string | null,
+              public password: string | null,
+              public email: string | null,
               public role: string | null,
-              public age: number | null) {
+              public age: number | string | null) {
   }
 }
